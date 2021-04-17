87874b21be87479f5ce5f482d6b6beaec732b8d2
Schalk Olivier <schalkolivier@vegamine.com>
2021-04-17 16:01:34 +0200

    Added trip components

---
+++ src/components/trips/TripListItem.vue
@@ -0,0 +1,62 @@
+<template>
+    <ion-item>
+        <ion-label>
+            <h2>{{ fmtDate(trip.date) }}</h2>
+            <p>{{ trip.description }} ({{ trip.totalDistance }}Km)</p>
+        </ion-label>
+
+        <ion-icon slot="end" :src="require('../../../public/assets/icon/trash-outline.svg')" @click="deleteTrip(trip.id)"></ion-icon>
+    </ion-item>
+</template>
+
+<script>
+import { IonIcon, IonItem, IonLabel, loadingController, toastController } from '@ionic/vue';
+import { mapActions } from 'vuex';
+import moment from 'moment';
+
+
+export default {
+    props: ['trip'],
+    components: {
+        IonItem, IonLabel, IonIcon
+    },
+    methods: {
+        ...mapActions({
+            deleteById: 'deleteTrip'
+        }),
+        fmtDate(timestamp) {
+            const date = new Date(timestamp.seconds * 1000)
+            return moment(date).format('YYYY/MM/DD hh:mm A')
+        },
+        deleteTrip(id){
+            loadingController.create({
+                message: 'Please Wait...'
+            }).then((loading) => {
+                loading.present();
+                this.deleteById(id)
+                .then(() => {
+                    loading.dismiss();
+                    toastController.create({
+                        message: "Deleted",
+                        duration: 1000,
+                        color: 'success',
+                        position: 'top'
+                    }).then((toast) => {
+                        toast.present()
+                    })
+                }).catch(() => {
+                    loading.dismiss();
+                    toastController.create({
+                        message: "Username or Password incorrect",
+                        duration: 1000,
+                        color: 'danger',
+                        position: 'top'
+                    }).then((toast) => {
+                        toast.present()
+                    })
+                })
+            });
+        }
+    }
+}
+</script>
---
+++ src/components/trips/TripsList.vue
@@ -0,0 +1,27 @@
+<template>
+    <ion-list class="ion-padding">
+        <ion-list-header>
+            <h2>Trips</h2>
+        </ion-list-header>
+        <div v-if="trips.length != 0">
+            <trip-list-item v-for="trip in trips" :key="trip.id" :trip="trip"></trip-list-item>
+        </div>
+        <ion-item v-else>
+            <ion-label class="ion-text-center">
+                No trip data yet...
+            </ion-label>
+        </ion-item>
+    </ion-list>
+</template>
+
+<script>
+import { IonList, IonListHeader, IonItem, IonLabel } from '@ionic/vue';
+import TripListItem from '@/components/trips/TripListItem.vue';
+
+export default {
+    props: ['trips'],
+    components: {
+        IonList, IonListHeader, TripListItem, IonItem, IonLabel
+    }
+}
+</script>
